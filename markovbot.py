fc5c35ba22b587a4905df677572729e3d62292db
Martin Ramsay <mgramsay.dev@gmail.com>
2016-12-10 02:49:59 +0000

    Fix bug for if a row in the matrix is all zeroes
    If the sample text is short, the probability distribution for the next
    words following the last n-gram in the text may be all zero. In this
    case we simply start with a new random initial n-gram.

--- markovbot.py
+++ markovbot.py
@@ -12,12 +12,23 @@ import ngrams
 def get_next_index(last_word, new_word, ngram_list, t_matrix):
     current_ngram = ngrams.build_ngram(last_word, new_word)
     ngram_index = ngram_list.index(current_ngram)
+    if sum(t_matrix.matrix[ngram_index]) < 0.5:
+        return -1
     next_word_index = arb_random.get_random_index(t_matrix.matrix[ngram_index])
     return next_word_index
 
+def random_start(start_ngrams, start_probs):
+    '''
+    Pick a random starting n-gram, based on frequency analysis of the sampled
+    text
+    '''
+    start_ind = arb_random.get_random_index(start_probs)
+    last_word, new_word = ngrams.split_ngram(start_ngrams[start_ind])
+    return last_word, new_word
+
 def main():
     tweet = []
-    text_file = 'example_obama.txt'
+    text_file = 'test.txt'
     full_text = sample_text.read_string_list(text_file)
     ngram_list = sample_text.build_ngram_list(full_text)
     t_matrix, start_ngrams, start_probs, word_list = \
@@ -27,17 +38,18 @@ def main():
     while remaining_chars > 0:
         sentence = []
         if len(tweet) == 0:
-            start_ind = arb_random.get_random_index(start_probs)
-            # Pick a random starting n-gram, based on frequency analysis of
-            # the sampled text
-            last_word, new_word = ngrams.split_ngram(start_ngrams[start_ind])
+            last_word, new_word = random_start(start_ngrams, start_probs)
             sentence.append(last_word)
         else:
             next_word_index = get_next_index(last_word, new_word, ngram_list,
                                              t_matrix)
             # For any sentences after the first, just use the transition matrix
-            last_word = new_word
-            new_word = word_list[next_word_index]
+            if next_word_index == -1:
+                last_word, new_word = random_start(start_ngrams, start_probs)
+                sentence.append(last_word)
+            else:
+                last_word = new_word
+                new_word = word_list[next_word_index]
         if new_word not in ngrams.PUNCTUATION:
             sentence.append(' ')
         sentence.append(new_word)
